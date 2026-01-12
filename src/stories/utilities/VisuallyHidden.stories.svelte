28ee9a7127409b3d85d58fd2a81c1e5f575e2dd4
Jacob Weinbren <jacobweinbren@gmail.com>
2026-01-12 04:56:24 +0000

    Simplified Visually Hidden

--- src/stories/utilities/VisuallyHidden.stories.svelte
+++ src/stories/utilities/VisuallyHidden.stories.svelte
@@ -2,9 +2,6 @@
 	import { defineMeta } from "@storybook/addon-svelte-csf";
 	import { VisuallyHidden } from "$saas/utilities/visually-hidden";
 	import { Button } from "$saas/components/button";
-	import { Input } from "$saas/components/input";
-	import { Stack } from "$saas/layout/stack";
-	import { Text } from "$saas/typography/text";
 
 	const { Story } = defineMeta({
 		title: "utilities/VisuallyHidden",
@@ -35,36 +32,4 @@
 	</Button>
 {/snippet}
 
-{#snippet inputStory()}
-	<Stack class="max-w-sm">
-		<label for="hidden-input">
-			<VisuallyHidden>Email address</VisuallyHidden>
-		</label>
-		<Input id="hidden-input" placeholder="Enter your email" type="email" />
-		<Text size="sm" class="text-zinc-500">
-			The input label is hidden but accessible to screen readers
-		</Text>
-	</Stack>
-{/snippet}
-
-{#snippet skipLinkStory()}
-	<Stack>
-		<a
-			href="#main-content"
-			class="rounded bg-blue-600 px-4 py-2 text-white focus:not-sr-only focus:absolute focus:left-4 focus:top-4 focus:z-50"
-		>
-			<VisuallyHidden>Skip to main content</VisuallyHidden>
-		</a>
-		<Text size="sm" class="text-zinc-500">
-			Tab to see the skip link appear (try pressing Tab key)
-		</Text>
-		<div id="main-content" class="rounded border border-zinc-200 p-4">
-			<Text size="lg" weight="semibold">Main Content</Text>
-			<Text>This is the main content area.</Text>
-		</div>
-	</Stack>
-{/snippet}
-
 <Story name="Basic" template={basicStory} />
-<Story name="With Input" template={inputStory} />
-<Story name="Skip Link" template={skipLinkStory} />
--- templates/utilities/visually-hidden/visually-hidden.svelte
+++ templates/utilities/visually-hidden/visually-hidden.svelte
@@ -15,9 +15,6 @@
 	let { children, class: className }: Props = $props();
 </script>
 
-<span
-	class="absolute h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 {className}"
-	style="clip: rect(0, 0, 0, 0); clip-path: inset(50%);"
->
+<span class="sr-only {className}">
 	{@render children()}
 </span>
