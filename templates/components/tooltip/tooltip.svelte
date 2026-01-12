67ae7c2252e02f1eccf2ef00c7317fbab7420870
Jacob Weinbren <jacobweinbren@gmail.com>
2026-01-12 05:32:16 +0000

    Update tooltip.svelte

--- templates/components/tooltip/tooltip.svelte
+++ templates/components/tooltip/tooltip.svelte
@@ -6,7 +6,7 @@
 			content: [
 				"z-50 rounded px-2.5 py-1 shadow-sm",
 				"animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
-				"data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+
 				"data-[side=bottom]:origin-top data-[side=left]:origin-right data-[side=right]:origin-left data-[side=top]:origin-bottom",
 				"bg-white dark:bg-zinc-950",
 				"text-zinc-950 dark:text-neutral-50",
