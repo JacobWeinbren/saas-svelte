2a0b13824ca0c6b3c940bfd9caab14b730b69d88
Jacob Weinbren <jacobweinbren@gmail.com>
2026-01-16 11:12:47 +0000

    Removed unused components

--- templates/components/toast/index.ts
+++ templates/components/toast/index.ts
@@ -1,9 +1,4 @@
 export { default as Toast } from "./toast.svelte";
-export { default as ToastRoot } from "./toast-root.svelte";
 export { default as Toaster } from "./toaster.svelte";
-export { default as ToastTitle } from "./toast-title.svelte";
-export { default as ToastDescription } from "./toast-description.svelte";
-export { default as ToastCloseTrigger } from "./toast-close-trigger.svelte";
-export { default as ToastActionTrigger } from "./toast-action-trigger.svelte";
 export { createToaster } from "@ark-ui/svelte/toast";
 export type { CreateToasterReturn } from "@ark-ui/svelte/toast";
