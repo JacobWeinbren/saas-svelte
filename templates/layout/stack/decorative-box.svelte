678d09a651946630b5b2480e2be8337cbcff59ae
Jacob Weinbren <jacobweinbren@gmail.com>
2026-01-21 16:56:27 +0000

    Update decorative-box.svelte

--- templates/layout/stack/decorative-box.svelte
+++ templates/layout/stack/decorative-box.svelte
@@ -11,7 +11,7 @@
 
 <div
 	class={twMerge(
-		"flex items-center justify-center border",
+		"flex items-center justify-center bg-bg-emphasized",
 		// Dot Pattern Effect
 		"bg-[url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0nNicgaGVpZ2h0PSc2JyB2aWV3Qm94PScwIDAgNiA2JyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnPjxnIGZpbGw9JyM5QzkyQUMnIGZpbGwtb3BhY2l0eT0nMC4yJyBmaWxsLXJ1bGU9J2V2ZW5vZGQnPjxwYXRoIGQ9J001IDBoMUwwIDZWNXpNNiA1djFINXonLz48L2c+PC9zdmc+)] bg-clip-padding",
 		className,
